b72f6304c4936f0f92e3893d8266fcf6398534fd
Jesse Washburn <jesse.washburn@gmail.com>
2015-10-24 23:54:53 -0400

    Use environment variables, but set fall-back defaults.
    In addition to adding fall-back default environment variables, use the more promiscuous .rstrip() method. Whether the newline character(s) are \n, \r, or both, they need to be stripped. The method .rstrip(os.linesep) leaves the possibility that a newline character will remain attached to the string.

--- sqspush.py
+++ sqspush.py
@@ -4,14 +4,14 @@ import sys
 import boto.sqs
 import os
 
-sqs = boto.sqs.connect_to_region(os.environ['AWS_DEFAULT_REGION'])
-queue = sqs.create_queue(os.environ['SQS_QUEUE'])
+sqs = boto.sqs.connect_to_region(os.getenv('AWS_DEFAULT_REGION'], 'us-east-1'))
+queue = sqs.create_queue(os.getenv('SQS_QUEUE', 'dump1090'))
 
 messages = list()
 batch = list()
 
 for line in sys.stdin:
-  messages.append( line.rstrip(os.linesep) )
+  messages.append( line.rstrip() )
   if len(messages) == 10:
     # print ';'.join(messages)
     for i, message in enumerate(messages):
